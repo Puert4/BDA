c0c11da90ac00aa6cb330dee6049570505d237c5
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-02-19 22:40:34 -0700

    Correccion de nombre de clase

--- CajeroView/src/main/java/com/mycompany/cajeroview/RetiroSinCuentaForm.java
+++ CajeroView/src/main/java/com/mycompany/cajeroview/RetiroSinCuentaForm.java
@@ -8,12 +8,12 @@ package com.mycompany.cajeroview;
  *
  * @author TeLesheo
  */
-public class RetirnoSinCuentaForm extends javax.swing.JDialog {
+public class RetiroSinCuentaForm extends javax.swing.JDialog {
 
     /**
      * Creates new form RetirnoSinCuentaForm
      */
-    public RetirnoSinCuentaForm(java.awt.Frame parent, boolean modal) {
+    public RetiroSinCuentaForm(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -138,20 +138,21 @@ public class RetirnoSinCuentaForm extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RetirnoSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RetiroSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RetirnoSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RetiroSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RetirnoSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RetiroSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RetirnoSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RetiroSinCuentaForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                RetirnoSinCuentaForm dialog = new RetirnoSinCuentaForm(new javax.swing.JFrame(), true);
+                RetiroSinCuentaForm dialog = new RetiroSinCuentaForm(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
